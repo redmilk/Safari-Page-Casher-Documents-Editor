d46d29aed6a5115cb4bbcaf13c46a66f776b9a0c
Danyl Timofeyev <timofeev.danil@gmail.com>
2021-11-18 11:30:15 +0200

    Commit initial project structure

---
+++ Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Jiggle
+//
+//  Created by Danyl Timofeyev on 18.11.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+    
+}
+
